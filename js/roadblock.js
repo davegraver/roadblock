d949a0b61cd4dfe27c85c5e4cb1c2a4db1f27e41
Dave Graver <dgraver@shint.com>
2014-12-11 19:20:56 -0500

    Updating js so roadblock isn't dismissed on page refresh.

--- js/roadblock.js
+++ js/roadblock.js
@@ -1,29 +1,31 @@
 jQuery(document).ready(function() {
-	// Check if they've been shown the roadblock already
-	if (! $.cookie('roadblock_ad')) {
-		$('body').append('<div class="overlay"></div>');
-		$('.roadblock').css('display', 'block');
-		$('.content').append('<a href="#" title="Close" class="close"><i class="fa fa-times-circle"></i><span class="sr-only">Close</span></a>');
+    // Check if they've been shown the roadblock already
+    if (! $.cookie('roadblock_ad')) {
+        $('body').append('<div class="overlay"></div>');
+        $('.roadblock').css('display', 'block');
+        $('.content').append('<a href="#" title="Close" class="close"><i class="fa fa-times-circle"></i><span class="sr-only">Close</span></a>');
 
-		// Ad Click
-		$('.roadblock-link').click(function() {
-			$('.overlay').remove();
-			$('.roadblock').css('display', 'none');
+        // Ad Click
+        $('.roadblock-link').click(function() {
+            $('.overlay').remove();
+            $('.roadblock').css('display', 'none');
 
-			// Track event
-			// pageTracker._trackEvent('Ad Promo Name', 'Clicked Ad');
-		});
+            // Track event
+            // pageTracker._trackEvent('Ad Promo Name', 'Clicked Ad');
 
-		// Close Click
-		$('.close').click(function(event) {
-			$('.overlay').remove();
-			$('.roadblock').css('display', 'none');
+            $.cookie('roadblock_ad', 'TRUE', { path: '/', expires: 1000 });
+        });
 
-			// Track event
-			// pageTracker._trackEvent('Ad Promo Name', 'Closed Ad');
-			return false;
-		});
+        // Close Click
+        $('.close').click(function(event) {
+            $('.overlay').remove();
+            $('.roadblock').css('display', 'none');
 
-	$.cookie('roadblock_ad', 'TRUE', { path: '/', expires: 1000 });
-	}
+            // Track event
+            // pageTracker._trackEvent('Ad Promo Name', 'Closed Ad');
+
+            $.cookie('roadblock_ad', 'TRUE', { path: '/', expires: 1000 });
+            return false;
+        });
+    }
 });
